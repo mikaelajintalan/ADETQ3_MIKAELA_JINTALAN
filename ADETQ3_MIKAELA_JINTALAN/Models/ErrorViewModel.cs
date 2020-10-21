1b7408f3a3dbf27d61f475678172f10402d97c73
MIKAELA JINTALAN <1169-20@itmlyceumalabang.onmicrosoft.com>
2020-10-21 19:35:32 +0800

    Q3

---
+++ ADETQ3_MIKAELA_JINTALAN/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ADETQ3_MIKAELA_JINTALAN.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
